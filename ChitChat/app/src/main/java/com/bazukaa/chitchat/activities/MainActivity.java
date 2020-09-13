22357a5bad1d4b262f4fc49565b9c49fe28f1086
nakshatra-bazukaa <nakshatravasugupta@gmail.com>
2020-09-13 23:24:04 +0530

    Sign in/out implemented

--- ChitChat/app/src/main/java/com/bazukaa/chitchat/activities/MainActivity.java
+++ ChitChat/app/src/main/java/com/bazukaa/chitchat/activities/MainActivity.java
@@ -1,16 +1,21 @@
 package com.bazukaa.chitchat.activities;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.TextView;
-
+import android.widget.Toast;
+import androidx.appcompat.app.AppCompatActivity;
 import com.bazukaa.chitchat.R;
 import com.bazukaa.chitchat.util.Constants;
 import com.bazukaa.chitchat.util.PreferenceManager;
-
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FieldValue;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.iid.FirebaseInstanceId;
+import java.util.HashMap;
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -25,6 +30,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
 
+        // To set user name on the header
         preferenceManager = new PreferenceManager(this);
         tvTitle.setText(String.format(
                 "%s %s",
@@ -32,6 +38,46 @@ public class MainActivity extends AppCompatActivity {
                 preferenceManager.getString(Constants.KEY_LAST_NAME)
         ));
 
-
+        // To update fcm token in db
+        FirebaseInstanceId.getInstance().getInstanceId().addOnCompleteListener(task -> {
+            if(task.isSuccessful() && task.getResult() != null)
+                sendFCMTokenToDatabase(task.getResult().getToken());
+        });
+    }
+    // TO sign out
+    @OnClick(R.id.act_main_tv_signout)
+    public void onSignOutClicked(){
+        signOut();
+    }
+    private void sendFCMTokenToDatabase(String token){
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        DocumentReference documentReference =
+                db.collection(Constants.KEY_COLLECTION_USERS).document(
+                        preferenceManager.getString(Constants.KEY_USER_ID)
+                );
+        documentReference.update(Constants.KEY_FCM_TOKEN, token)
+                .addOnSuccessListener(aVoid -> {
+                    Toast.makeText(this, "Token updated", Toast.LENGTH_SHORT).show();
+                })
+                .addOnFailureListener(e -> {
+                    Toast.makeText(this, "Unable to send token" + e.getMessage(), Toast.LENGTH_SHORT).show();
+                });
+    }
+    private void signOut(){
+        Toast.makeText(this, "Signing out...", Toast.LENGTH_SHORT).show();
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        DocumentReference documentReference =
+                db.collection(Constants.KEY_COLLECTION_USERS).document(
+                        preferenceManager.getString(Constants.KEY_USER_ID)
+                );
+        HashMap<String, Object> updates = new HashMap<>();
+        updates.put(Constants.KEY_FCM_TOKEN, FieldValue.delete());
+        documentReference.update(updates)
+                .addOnSuccessListener(aVoid -> {
+                    preferenceManager.clearPreferences();
+                    startActivity(new Intent(getApplicationContext(), SignInActivity.class));
+                    finish();
+                })
+                .addOnFailureListener(e -> Toast.makeText(MainActivity.this, "Unable to sign out", Toast.LENGTH_SHORT).show());
     }
 }
